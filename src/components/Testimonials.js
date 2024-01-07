47ae876faa8a4edb7f6bb4cdd920d77213ee0c84
Kevin Agoh <kevinagoh@hotmail.fr>
2024-01-08 00:59:43 +0100

    Added e commerce site on portfolio

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
-import { testimonials } from "../data";
+import React from 'react';
+import { TerminalIcon, UsersIcon } from '@heroicons/react/solid';
+import { testimonials } from '../data';
 
 export default function Testimonials() {
   return (
-    <section id="testimonials">
-      <div className="container px-5 py-10 mx-auto text-center">
-        <UsersIcon className="w-10 inline-block mb-4" />
-        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-          Client Testimonials
+    <section id='testimonials'>
+      <div className='container px-5 py-10 mx-auto text-center'>
+        <UsersIcon className='w-10 inline-block mb-4' />
+        <h1 className='sm:text-4xl text-3xl font-medium title-font text-white mb-12'>
+          Testimonials
         </h1>
-        <div className="flex flex-wrap m-4">
-          {testimonials.map((testimonial) => (
-            <div className="p-4 w-full">
-              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
-                <div className="inline-flex items-center">
+        <div className='flex flex-wrap m-4'>
+          {testimonials.map(testimonial => (
+            <div className='p-4 w-full'>
+              <div className='h-full bg-gray-800 bg-opacity-40 p-8 rounded'>
+                <TerminalIcon className='block w-8 text-gray-500 mb-4' />
+                <p className='leading-relaxed mb-6'>{testimonial.quote}</p>
+                <div className='inline-flex items-center'>
                   {testimonial.image && (
                     <img
-                      alt="testimonial"
+                      alt='testimonial'
                       src={testimonial.image}
-                      className="w-12 rounded-full flex-shrink-0 object-cover object-center"
+                      className='w-12 rounded-full flex-shrink-0 object-cover object-center'
                     />
                   )}
-                  <span className="flex-grow flex flex-col pl-4">
+                  <span className='flex-grow flex flex-col pl-4'>
                     <a
                       href={testimonial.linkedIn}
-                      className="title-font font-medium text-white"
+                      className='title-font font-medium text-white'
                     >
                       {testimonial.name}
                     </a>
-                    <span className="text-gray-500 text-sm uppercase">
+                    <span className='text-gray-500 text-sm uppercase'>
                       {testimonial.company}
                     </span>
                   </span>
